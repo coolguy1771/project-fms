77d5c463c5ad4940b2acf4c72b742674006e8e98
aniketgarg <89074560+aniketgarg1@users.noreply.github.com>
2022-11-09 13:56:45 -0700

    updared canvas

--- src/routes/exercise/Test/+page.svelte
+++ src/routes/exercise/Test/+page.svelte
@@ -5,36 +5,37 @@
     function done(exercise){
       console.log(exercise)
     }
+  
     const sketch = (p5) => {
       let bgcolor = 'white';
-  
+      let sel = p5.createSelect();
       p5.setup = () => {
         p5.createCanvas(900, 600);
         p5.background(bgcolor);
         p5.setupCanvas();
+        console.log('setup done')
       }
-      p5.setupCanvas = ()=> {
+      p5.setupCanvas = () => {
         p5.noStroke();
-        p5.colorPicker = p5.createColorPicker('#000000');
-        p5.colorPicker.position(10, 50);
+        let colorPicker = p5.createColorPicker('#000000');
+        colorPicker.position(10, 50);
 
-        p5.sel = createSelect();
-        p5.sel.position(150, 50);
-        p5.sel.option("Normal Paint Brush (press 'N')");
-        p5.sel.option("Splatter Brush (press 'S')");
-        p5.sel.option("Eraser (press 'E')");
-        p5.sel.option("Draw Rectangle (press 'Q')");
-        p5.sel.option("Draw Triangle (press 'T')");
+        sel.position(150, 50);
+        sel.option("Normal Paint Brush (press 'N')");
+        sel.option("Splatter Brush (press 'S')");
+        sel.option("Eraser (press 'E')");
+        sel.option("Draw Rectangle (press 'Q')");
+        sel.option("Draw Triangle (press 'T')");
 
         //create a slider
-        p5.slider = p5.createSlider(1, 100, 20, 1);
-        p5.slider.position(370, 50);
-        p5.slider.style('width', '180px');
+        let slider = p5.createSlider(1, 100, 20, 1);
+        slider.position(370, 50);
+        slider.style('width', '180px');
 
-        p5.button = p5.createButton("Erase Canvas (press 'R')");
-        p5.button.position(650, 50);
-        p5.button.mousePressed(clearBG);
-
+        let button = p5.createButton("Erase Canvas (press 'R')");
+        button.position(650, 50);
+        button.mousePressed(p5.clearBG);
+        console.log('canvas setup')
       }
 
 
@@ -44,7 +45,7 @@
           
         p5.noStroke();
         p5.fill('#d3d3d3');
-        p5.rect(0, 0, width, 100);
+        p5.rect(0, 0, p5.width, 100);
 
         p5.fill('black');
         p5.textSize(16);
@@ -59,24 +60,24 @@
         p5.text("Color of brush \n (press 'C' for a \n random color):", 10, 15);
 
         if (p5.mouseIsPressed && p5.mouseY > 100) {
-          if (p5.sel.value() == "Normal Paint Brush (press 'N')") {
+          if (sel.value() == "Normal Paint Brush (press 'N')") {
 
-            stroke(p5.colorPicker.color());
-            strokeWeight(p5.slider.value());
-            line(p5.pmouseX, p5.pmouseY, p5.mouseX, p5.mouseY);
+            p5.stroke(colorPicker.color());
+            p5.strokeWeight(slider.value());
+            p5.line(p5.pmouseX, p5.pmouseY, p5.mouseX, p5.mouseY);
           }
-          if (p5.sel.value() == "Splatter Brush (press 'S')") {
+          if (sel.value() == "Splatter Brush (press 'S')") {
 
-            for (i = 0; i < random(1, 10); i++) {
+            for (i = 0; i < p5.random(1, 10); i++) {
               
               p5.noStroke();
 
-              p5.fill(p5.colorPicker.color());
+              p5.fill(colorPicker.color());
 
-              p5.ellipse(p5.mouseX + random(-100, 100), p5.mouseY + random(-100, 100), p5.slider.value(), p5.slider.value());
+              p5.ellipse(p5.mouseX + p5.random(-100, 100), p5.mouseY + p5.random(-100, 100), slider.value(), slider.value());
             }
           }
-          if (p5.sel.value() == "Eraser (press 'E')") {
+          if (sel.value() == "Eraser (press 'E')") {
             //eraser
 
             //draw a line in background color
@@ -84,73 +85,68 @@
             p5.strokeWeight(slider.value());
             p5.line(p5.pmouseX, p5.pmouseY, p5.mouseX, p5.mouseY);
           }
-          if (p5.sel.value() == "Draw Rectangle (press 'Q')") {
+          if (sel.value() == "Draw Rectangle (press 'Q')") {
             //draw rectangle with brush thickness at mousex and y
-            p5.fill(p5.colorPicker.color());
-            p5.rect(p5.mouseX, p5.mouseY, p5.slider.value(), p5.slider.value());
+            p5.fill(colorPicker.color());
+            p5.rect(p5.mouseX, p5.mouseY, slider.value(), slider.value());
           }
-          if (p5.sel.value() == "Draw Triangle (press 'T')") {
+          if (sel.value() == "Draw Triangle (press 'T')") {
             
-            p5.fill(p5.colorPicker.color());
-            p5.triangle(p5.mouseX, p5.mouseY, p5.mouseX + p5.slider.value(), p5.mouseY + p5.slider.value(), p5.mouseX - p5.slider.value(), p5.mouseY + p5.slider.value());
+            p5.fill(colorPicker.color());
+            p5.triangle(p5.mouseX, p5.mouseY, p5.mouseX + slider.value(), p5.mouseY + slider.value(), p5.mouseX - slider.value(), p5.mouseY + slider.value());
           }
         }
       }
+    
       p5.clearBG=()=> { 
         p5.fill(bgcolor);
         p5.noStroke();
-        p5.rect(0, 100, width, height - 100);
+        p5.rect(0, 100, p5.width, p5.height - 100);
       }
       p5.keyPressed=()=> {
 
-        if (key == 'n' || key == 'N') {
+        if (p5.key == 'n' || p5.key == 'N') {
           
-          p5.sel.selected("Normal Paint Brush (press 'N')");
-        } else if (key == 's' || key == 'S') {
+          sel.selected("Normal Paint Brush (press 'N')");
+        } else if (p5.key == 's' || p5.key == 'S') {
           
-          p5.sel.selected("Splatter Brush (press 'S')");
-        } else if (key == 'e' || key == 'E') {
+          sel.selected("Splatter Brush (press 'S')");
+        } else if (p5.key == 'e' || p5.key == 'E') {
           
-          p5.sel.selected("Eraser (press 'E')");
-        } else if (key == '+') {
+          sel.selected("Eraser (press 'E')");
+        } else if (p5.key == '+') {
 
-          p5.slider.value(slider.value() + 1);
-        } else if (key == '-') {
+          slider.value(slider.value() + 1);
+        } else if (p5.key == '-') {
           
-          p5.slider.value(slider.value() - 1);
-        } else if (key == 'r' || key == 'R') {
+          slider.value(slider.value() - 1);
+        } else if (p5.key == 'r' || p5.key == 'R') {
 
           p5.clearBG();
-        } else if (key == 'c' || key == 'C') {
+        } else if (p5.key == 'c' || p5.key == 'C') {
           
-          let randomColor = "#" + p5.Math.floor(Math.random() * 16777215).toString(16);
-          p5.colorPicker = p5.createColorPicker(randomColor);
-          p5.colorPicker.position(10, 50);
-        } else if (key == 'q'|| key == 'Q'){
+          let randomColor = "#" + p5.Math.floor(p5.Math.random() * 16777215).toString(16);
+          let colorPicker = p5.createColorPicker(randomColor);
+          colorPicker.position(10, 50);
+        } else if (p5.key == 'q'|| p5.key == 'Q'){
 
-          p5.sel.selected("Draw Rectangle (press 'Q')");
-        }else if (key == 't'|| key == 'T'){
+          sel.selected("Draw Rectangle (press 'Q')");
+        }else if (p5.key == 't'|| p5.key == 'T'){
         
-          p5.sel.selected("Draw Triangle (press 'T')");
+          sel.selected("Draw Triangle (press 'T')");
         }
 
       }
-
+    }
   </script>
   
   <div class="content">
-    <div class="p5Canvas">
       <P5 {sketch} />
-    </div>
-  
-    
   </div>
   
   <style>
-    .p5Canvas {
+    .content {
       border: 1px solid #ed225d;
-      width: 100%;
-      height: 100%;
     }
     button {
       position: absolute;
